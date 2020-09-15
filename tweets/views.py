4dfe673df38e97d37ef2bc0916d4e2203a1f8c39
fmarcinek <fmarcinek15@gmail.com>
2020-09-15 12:41:53 +0200

    Tweet creating form on the home page (with redirection)

--- tweets/views.py
+++ tweets/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404, JsonResponse
+from django.utils.http import is_safe_url
+from django.conf import settings
 
 from .models import Tweet
 from .forms import TweetForm
@@ -25,9 +27,12 @@ def tweet_detail_view(request, tweet_id):
 
 def tweet_create_view(request):
     form = TweetForm(request.POST or None)
+    next_url = request.POST.get('next') or None
     if form.is_valid():
         obj = form.save(commit=False)
         obj.save()
+        if next_url is not None and is_safe_url(next_url, allowed_hosts=settings.ALLOWED_HOSTS):
+            return redirect(next_url)
         form = TweetForm()
     return render(request, 'components/form.html', context={'form': form})
 

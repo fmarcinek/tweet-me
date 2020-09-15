9c3247b23ea53d90f1b0ac2a91ad5b24aa664eca
fmarcinek <fmarcinek15@gmail.com>
2020-09-15 18:59:16 +0200

    Fix non-displaying tweet when created

--- tweets/views.py
+++ tweets/views.py
@@ -27,12 +27,11 @@ def tweet_detail_view(request, tweet_id):
 
 def tweet_create_view(request):
     form = TweetForm(request.POST or None)
-    next_url = request.POST.get('next') or None
     if form.is_valid():
         obj = form.save(commit=False)
         obj.save()
         if request.is_ajax():
-            return JsonResponse({obj.serialize()}, status=201)  # 201 is for created items
+            return JsonResponse(obj.serialize(), status=201)  # 201 is for created items
         form = TweetForm()
     return render(request, 'components/form.html', context={'form': form})
 

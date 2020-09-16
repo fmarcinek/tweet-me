60a75dbb4e9ed6b17c7e938b4b16ca9e539f69c0
fmarcinek <fmarcinek15@gmail.com>
2020-09-16 11:01:32 +0200

    Move tweet detail and list views to DRF serializers

--- tweets/views.py
+++ tweets/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, Http404, JsonResponse
-from django.utils.http import is_safe_url
+from django.http import JsonResponse
 from django.conf import settings
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 from .models import Tweet
 from .forms import TweetForm
@@ -12,7 +13,17 @@ def home(request):
     return render(request, 'pages/home.html', context={}, status=200)
 
 
+@api_view(['GET'])
 def tweet_detail_view(request, tweet_id):
+    qs = Tweet.objects.filter(id=tweet_id)
+    if not qs.exists():
+        return Response({}, status=404)
+    obj = qs.first()
+    serializer = TweetSerializer(obj)
+    return Response(serializer.data)
+
+
+def tweet_detail_view_pure_django(request, tweet_id):
     data = {
         "id": tweet_id,
     }
@@ -26,12 +37,13 @@ def tweet_detail_view(request, tweet_id):
     return JsonResponse(data, status=status)
 
 
+@api_view(['POST'])
 def tweet_create_view(request):
-    serializer = TweetSerializer(data=request.POST or None)
-    if serializer.is_valid():
+    serializer = TweetSerializer(data=request.POST)
+    if serializer.is_valid(raise_exception=True):
         serializer.save(user=request.user)
-        return JsonResponse(serializer.data, status=201)
-    return JsonResponse({}, status=400)
+        return Response(serializer.data, status=201)
+    return Response({}, status=400)
 
 
 def tweet_create_view_pure_django(request):
@@ -53,7 +65,14 @@ def tweet_create_view_pure_django(request):
     return render(request, 'components/form.html', context={'form': form})
 
 
+@api_view(['GET'])
 def tweet_list_view(request):
+    qs = Tweet.objects.all()
+    serializer = TweetSerializer(qs, many=True)
+    return Response(serializer.data)
+
+
+def tweet_list_view_pure_django(request):
     qs = Tweet.objects.all()
     tweets_list = [q.serialize() for q in qs]
     data = {
